6321b77a2923d93f3025890e01cad079f089d415
Cameron Cundiff <pair+ccundiff@pivotallabs.com>
2014-02-18 11:57:44 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HologramRailsExample::Application.config.secret_key_base = '3942eb8586094908e1d648a6c206ccb56c68a9f585a9c9867ee144217ed3abe530295f4b2e6611bf3e51b6684738661940ad1b69db98495527c6a75801ac8fd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HologramRailsExample::Application.config.session_store :cookie_store, key: '_hologram-rails-example_session'
